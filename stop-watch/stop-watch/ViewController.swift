119111752fb18ebc37eb4c16c25da835a19774b2
goto <goto@ias2009001508k.i-asp.ne.jp>
2018-05-16 19:37:34 +0900

    goto's commit3

--- stop-watch/stop-watch/ViewController.swift
+++ stop-watch/stop-watch/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        // debug code goto 2
+        // debug code goto 3
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
